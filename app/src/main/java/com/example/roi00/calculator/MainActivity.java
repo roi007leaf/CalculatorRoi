6a4c255df9a97508a8fba8befad617165dc088ba
Roi Horowitz <roi007leaf@gmail.com>
2016-11-14 11:06:16 +0200

    Revert First Commit

--- app/src/main/java/com/example/roi00/calculator/MainActivity.java
+++ app/src/main/java/com/example/roi00/calculator/MainActivity.java
@@ -148,9 +148,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public String returnMyName(){
-        return "My Name Is Roi";
-    }
-
-
 }
